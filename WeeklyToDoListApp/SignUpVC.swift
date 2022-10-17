07f8e21cb34c4918450c48d15e1055896ed9aec1
Msnbek <mahmutsenbek@icloud.com>
2022-10-17 14:58:32 +0300

    Design completed ( %75 )

---
+++ WeeklyToDoListApp/SignUpVC.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpVC.swift
+//  WeeklyToDoListApp
+//
+//  Created by Mahmut Şenbek on 17.10.2022.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
